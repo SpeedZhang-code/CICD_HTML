14ca8d4c5092a23d5b6e99690963bc11e38360b0
SpeedZhang-code <wyzhang628@gmail.com>
2026-03-23 18:34:00 +0800

    update run build6

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 test('檢查網頁標題是否正確', () => {
     const html = fs.readFileSync('src/index.html', 'utf8');
-    expect(html).toMatch(/GTbooster/);
+    expect(html).toMatch(/GTBooster/);
 });
